7f08cb532c6dc889c98371eab04f8d125e7e4bc9
nitroluke <nitro.luke@gmail.com>
2014-12-02 18:58:07 -0700

    session 4 added

--- Session4.py
+++ Session4.py
@@ -1,3 +1,4 @@
+import math
 """
  12/1/14
  session 4 over Python topic on 9.10
@@ -5,9 +6,18 @@
 """
 
 # Exercise 6
-
+expression = 6 * (1 - 2)
+print(expression)
 
 # Exercise 8
-
+r = input("Please enter a radius to calculate the area of a circle > ")
+# pi * radius^2
+print("The area of the circle is ", math.pi * (int(r) ** 2))
 
 # Exercise 11
+
+#celcius to fahrenheit = c * (9/5) + 32
+
+c = float(input("What is the temp in celsius > "))
+f = c * (9 / 5) + 32
+print(c, "in celsius" , " = ", f , "in fahrenheit")
